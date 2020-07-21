f4ba9695a4c1bcc4aadb8c9d46e79abc84a6bdb4
Pamal-Ranasinghe <pamalofficial@gmail.com>
2020-07-21 18:09:16 +0530

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:worldtime/pages/home.dart';
+
+void main() =>  runApp(MaterialApp(
+
+  debugShowCheckedModeBanner: false,
+  home:Home(),
+));
+
+
+
