b936a0f62009d97cc8de08dea1f6271ecc1eedf0
Pamal-Ranasinghe <68419474+Pamal-Ranasinghe@users.noreply.github.com>
2020-07-21 18:15:34 +0530

    Update home.dart
    home screen name is changed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,7 +10,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Home screen')) ,
+      body: SafeArea(child: Text(' My Home screen')) ,
     );
   }
 }
